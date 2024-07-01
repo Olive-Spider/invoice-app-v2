4dc15bede154617b7d342050420abb52ff9b7ca4
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2024-07-01 16:36:41 +0300

    invoice UI improvements

--- resources/js/components/invoices/show.vue
+++ resources/js/components/invoices/show.vue
@@ -1,27 +1,51 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+let form = ref({ id: "" });
+
+const props = defineProps({
+    id: {
+        type: String,
+        default: "",
+    },
+});
+
+onMounted(async () => {
+    getInvoice();
+});
+const getInvoice = async () => {
+    let response = await axios.get(`/api/show_invoice/${props.id}`);
+    // console.log('form', response.data.invoice)
+    form.value = response.data.invoice;
+};
+
+const print = () => {
+    window.print()
+    router.push('/').catch(() => {})
+}
+</script>
+
 <template>
     <div class="container">
         <!--==================== SHOW INVOICE ====================-->
         <div class="invoices">
-
             <div class="card__header">
                 <div>
                     <h2 class="invoice__title">Invoice</h2>
                 </div>
-                <div>
-
-                </div>
+                <div></div>
             </div>
             <div>
-                <div class="card__header--title ">
-                    <h1 class="mr-2">#1043</h1>
-                    <p>July 17, 2020 at 3:28 am </p>
+                <div class="card__header--title">
+                    <h1 class="mr-2">{{ form.id }}</h1>
+                    <p>{{ form.created_at }}</p>
                 </div>
 
                 <div>
                     <ul class="card__header-list">
                         <li>
                             <!-- Select Btn Option -->
-                            <button class="selectBtnFlat">
+                            <button class="selectBtnFlat" @click="print()">
                                 <i class="fas fa-print"></i>
                                 Print
                             </button>
@@ -30,27 +54,26 @@
                         <li>
                             <!-- Select Btn Option -->
                             <button class="selectBtnFlat">
-                                <i class=" fas fa-reply"></i>
+                                <i class="fas fa-reply"></i>
                                 Edit
                             </button>
                             <!-- End Select Btn Option -->
                         </li>
                         <li>
                             <!-- Select Btn Option -->
-                            <button class="selectBtnFlat ">
-                                <i class=" fas fa-pencil-alt"></i>
+                            <button class="selectBtnFlat">
+                                <i class="fas fa-pencil-alt"></i>
                                 Delete
                             </button>
                             <!-- End Select Btn Option -->
                         </li>
-
                     </ul>
                 </div>
             </div>
 
             <div class="table invoice">
                 <div class="logo">
-                    <img src="/assets/img/logo.png" alt="" style="width: 200px;">
+                    <img src="/assets/img/logo.png" alt="" style="width: 200px" />
                 </div>
                 <div class="invoice__header--title">
                     <p></p>
@@ -58,35 +81,34 @@
                     <p></p>
                 </div>
 
-
                 <div class="invoice__header--item">
                     <div>
                         <h2>Invoice To:</h2>
-                        <p>Customer 1</p>
+                        <p v-if="form.customer">
+                            {{ form.customer.firstname }}
+                        </p>
                     </div>
                     <div>
                         <div class="invoice__header--item1">
                             <p>Invoice#</p>
-                            <span>#1200</span>
+                            <span>{{ form.number }}</span>
                         </div>
                         <div class="invoice__header--item2">
                             <p>Date</p>
-                            <span>12/12/2022</span>
+                            <span>{{ form.date }}</span>
                         </div>
                         <div class="invoice__header--item2">
                             <p>Due Date</p>
-                            <span>12/12/2022</span>
+                            <span>{{ form.due_date }}</span>
                         </div>
                         <div class="invoice__header--item2">
                             <p>Reference</p>
-                            <span>1045</span>
+                            <span>{{ form.reference }}</span>
                         </div>
-
                     </div>
                 </div>
 
                 <div class="table py1">
-
                     <div class="table--heading3">
                         <p>#</p>
                         <p>Item Description</p>
@@ -96,14 +118,14 @@
                     </div>
 
                     <!-- item 1 -->
-                    <div class="table--items3">
-                        <p>1</p>
-                        <p>Lorem Ipsum is simply dummy text</p>
-                        <p>$ 300</p>
-                        <p>1</p>
-                        <p>$ 300</p>
+                    <div class="table--items3" v-for="(item,i) in form.invoice_items" :key="item.id">
+                        <p>{{i+1}}</p>
+                        <p>{{item.product.description}}</p>
+                        <p>$ {{item.unit_price}}</p>
+                        <p>{{item.quantity}}</p>
+                        <p>$ {{item.unit_price * item.quantity}}</p>
                     </div>
-                    <div class="table--items3">
+                    <!-- <div class="table--items3">
                         <p class="table--items--col2">
                             2
                         </p>
@@ -153,7 +175,7 @@
                         <p class="table--items--col5">
                             $ 300
                         </p>
-                    </div>
+                    </div> -->
                 </div>
 
                 <div class="invoice__subtotal">
@@ -163,45 +185,40 @@
                     <div>
                         <div class="invoice__subtotal--item1">
                             <p>Sub Total</p>
-                            <span> $ 1200</span>
+                            <span> $ {{form.sub_total}}</span>
                         </div>
                         <div class="invoice__subtotal--item2">
                             <p>Discount</p>
-                            <span>$ 100</span>
+                            <span>$ {{form.discount}}</span>
                         </div>
-
                     </div>
                 </div>
 
                 <div class="invoice__total">
                     <div>
                         <h2>Terms and Conditions</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                        <p>
+                            {{form.terms_and_conditions}}
+                        </p>
                     </div>
                     <div>
                         <div class="grand__total">
                             <div class="grand__total--items">
                                 <p>Grand Total</p>
-                                <span>$ 1100</span>
+                                <span>$ {{form.total}}</span>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-            <div class="card__footer">
+            <!-- <div class="card__footer">
+                <div></div>
                 <div>
-
+                    <a class="btn btn-secondary"> Save </a>
                 </div>
-                <div>
-                    <a class="btn btn-secondary">
-                        Save
-                    </a>
-                </div>
-            </div>
-
+            </div> -->
         </div>
 
-        <br><br><br>
+        <br /><br /><br />
     </div>
 </template>
